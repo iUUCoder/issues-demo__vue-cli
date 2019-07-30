272e0ced3a70f2624b735c980974fc8cc1290f21
UUCoder <uucoder@yeah.net>
2019-07-30 16:43:04 +0800

    reproduce issue.

---
+++ src/pages/home/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="page">
+        Hello World!
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.page {
+    color: red;
+    background-image: url("./res/image.png");
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pages: {
+    "pages/home": {
+      entry: "./src/pages/home/index.js",
+      template: "src/pages/home/index.html"
+    }
+  }
+};
